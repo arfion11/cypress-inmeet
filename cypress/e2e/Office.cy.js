905e0d1a5b55443ce5a89f8bb1424af94cee5231
arfion11 <diotama2arfi@gmail.com>
2025-03-09 14:34:11 +0700

    Script Add Office

--- cypress/e2e/Office.cy.js
+++ cypress/e2e/Office.cy.js
@@ -1,6 +1,6 @@
 ///<reference types="cypress" />
 
-describe('Office Add and Edit Tests', () => {
+describe('Office Add Tests', () => {
     const baseUrl = 'https://mris-staging.transtrack.id/';
   
     beforeEach(() => {
@@ -13,10 +13,116 @@ describe('Office Add and Edit Tests', () => {
         cy.visit('https://mris-staging.transtrack.id/offices');
       });
     });
-  
-    it('should allow user to access offices page directly without relogin', () => {
-        cy.visit('https://mris-staging.transtrack.id/offices');  
-        cy.url().should('include', '/offices'); 
+      it('Input add office valid', () => {
+        cy.visit('https://mris-staging.transtrack.id/offices');
+        cy.get('button[wire\\:click="openAddModal"]').click();
+    cy.get('#office_name').should('be.visible');
+    cy.get('#office_name').clear().type('TransTRACK');
+    cy.get('#region').should('be.visible');
+    const randomRegion = ['Malaysia', 'Singapore', 'Indonesia', 'Australia', 'Philippines'];
+    cy.get('#region').clear().type(randomRegion[Math.floor(Math.random() * randomRegion.length)]);
+    cy.get('#address').should('be.visible');
+    const randomAddress = [
+      'Jl. Emong No.24, Burangrang, Kota Bandung, Jawa Barat',
+      '123 Elm St, Springfield, IL 62701, United States',
+      '10 Downing St, London SW1A 2AA, United Kingdom',
+      '221B Baker St, London NW1 6XE, United Kingdom',
+      '1600 Pennsylvania Ave NW, Washington, DC 20500, United States'
+    ];
+    cy.get('#address').clear().type(randomAddress[Math.floor(Math.random() * randomAddress.length)]);
+        cy.get('button[wire\\:click="store"]').click();
+      });
+
+      it('check validasi office name tidak terisi', () => {
+        cy.visit('https://mris-staging.transtrack.id/offices');
+        cy.get('button[wire\\:click="openAddModal"]').click();
+    cy.get('#office_name').should('be.visible');
+    cy.get('#office_name').clear();
+    cy.get('#region').should('be.visible');
+    const randomRegion = ['Malaysia', 'Singapore', 'Indonesia', 'Australia', 'Philippines'];
+    cy.get('#region').clear().type(randomRegion[Math.floor(Math.random() * randomRegion.length)]);
+    cy.get('#address').should('be.visible');
+    const randomAddress = [
+      'Jl. Emong No.24, Burangrang, Kota Bandung, Jawa Barat',
+      '123 Elm St, Springfield, IL 62701, United States',
+      '10 Downing St, London SW1A 2AA, United Kingdom',
+      '221B Baker St, London NW1 6XE, United Kingdom',
+      '1600 Pennsylvania Ave NW, Washington, DC 20500, United States'
+    ];
+    cy.get('#address').clear().type(randomAddress[Math.floor(Math.random() * randomAddress.length)]);
+        cy.get('button[wire\\:click="store"]').click();
+        cy.contains('Office name is required.');
+      });
+
+      it('check validasi region tidak terisi', () => {
+        cy.visit('https://mris-staging.transtrack.id/offices');
+        cy.get('button[wire\\:click="openAddModal"]').click();
+    cy.get('#office_name').should('be.visible');
+    cy.get('#office_name').clear().type('TransTRACK');
+    cy.get('#region').should('be.visible');
+    const randomRegion = ['Malaysia', 'Singapore', 'Indonesia', 'Australia', 'Philippines'];
+    cy.get('#region').clear()
+    cy.get('#address').should('be.visible');
+    const randomAddress = [
+      'Jl. Emong No.24, Burangrang, Kota Bandung, Jawa Barat',
+      '123 Elm St, Springfield, IL 62701, United States',
+      '10 Downing St, London SW1A 2AA, United Kingdom',
+      '221B Baker St, London NW1 6XE, United Kingdom',
+      '1600 Pennsylvania Ave NW, Washington, DC 20500, United States'
+    ];
+    cy.get('#address').clear().type(randomAddress[Math.floor(Math.random() * randomAddress.length)]);
+        cy.get('button[wire\\:click="store"]').click();
+        cy.contains('Region is required.');
+      });
+
+      it('check validasi address tidak terisi', () => {
+        cy.visit('https://mris-staging.transtrack.id/offices');
+        cy.get('button[wire\\:click="openAddModal"]').click();
+    cy.get('#office_name').should('be.visible');
+    cy.get('#office_name').clear().type('TransTRACK');
+    cy.get('#region').should('be.visible');
+    const randomRegion = ['Malaysia', 'Singapore', 'Indonesia', 'Australia', 'Philippines'];
+    cy.get('#region').clear().type(randomRegion[Math.floor(Math.random() * randomRegion.length)]);
+    cy.get('#address').should('be.visible');
+    const randomAddress = [
+      'Jl. Emong No.24, Burangrang, Kota Bandung, Jawa Barat',
+      '123 Elm St, Springfield, IL 62701, United States',
+      '10 Downing St, London SW1A 2AA, United Kingdom',
+      '221B Baker St, London NW1 6XE, United Kingdom',
+      '1600 Pennsylvania Ave NW, Washington, DC 20500, United States'
+    ];
+    cy.get('#address').clear();
+        cy.get('button[wire\\:click="store"]').click();
+        cy.contains('Address is required.');
+      });
+
+      it('Mencegah penambahan office duplikat', () => {
+        cy.visit('https://mris-staging.transtrack.id/offices');
+        cy.get('button[wire\\:click="openAddModal"]').click();
+    cy.get('#office_name').should('be.visible');
+    cy.get('#office_name').clear().type('TransTRACK');
+    cy.get('#region').should('be.visible').type('Philippines');
+    cy.get('#address').should('be.visible').type('221B Baker St, London NW1 6XE, United Kingdom');;
+        cy.get('button[wire\\:click="store"]').click();
+        cy.contains('The office name has already been taken.');
       });
   });
   
+  describe('Office Edit Tests', () => {
+    const baseUrl = 'https://mris-staging.transtrack.id/';
+    beforeEach(() => {
+      cy.session('userSession', () => {
+        cy.visit(baseUrl);
+        cy.get('#emailOrUsername').type('admin'); 
+        cy.get('#password').type('Admin123');    
+        cy.get('#remember').check();
+        cy.get('button[type="submit"]').click();
+        cy.visit('https://mris-staging.transtrack.id/offices');
+      });
+    });
+
+    it('should edit office details successfully', () => {
+        cy.visit('https://mris-staging.transtrack.id/offices');
+        cy.get('button[wire\\:click="openEditModal(\'f88e56db-c8f3-4f94-8639-27a8c1f46c49\')"]').click(); 
+      });
+    });
